4bafbcf0a78a0ec522644eca55f949f0a4fe84db
John261106 <johnbobby2006@gmail.com>
2026-05-25 18:58:01 +0530

    modified core and added dispatcher

--- src/core/core.v
+++ src/core/core.v
@@ -1,4 +1,4 @@
-module core (
+module core #(parameter NUM_WARPS = 4)(
     input  wire clk,
     input  wire reset,
 
@@ -7,14 +7,54 @@ module core (
     input  wire [1:0]  warp_id_from_ms,
     input  wire [3:0]  lw_dest_in,
     input  wire [7:0]  lw_data_in  [0:15],
+    input  wire        lw_ready,
+    input  wire [1:0]  lw_warp_id,
 
     // to memory scheduler
     output wire        mem_req,
     output wire        mem_write,
     output wire [1:0]  warp_id_to_ms,
     output wire [7:0]  alu_result  [0:15],   // addresses for lw/sw
-    output wire [7:0]  rs2_out     [0:15]    // store data for sw
+    output wire [7:0]  rs2_out     [0:15],   // store data for sw
+    output wire [3:0]  lw_dest_out,          // Destination reg address
+
+    //Dispatcher Interface
+    input wire       warp_start        [0:NUM_WARPS-1],
+    input wire       warp_reset        [0:NUM_WARPS-1],
+    input wire [7:0] warp_id           [0:NUM_WARPS-1],
+    input wire [3:0] warp_thread_count [0:NUM_WARPS-1],
+
+    output wire warp_done[0:NUM_WARPS-1]
+
 );
+
+    //Latching Dispatcher Inputs
+    reg       warp_active             [0:NUM_WARPS-1];
+    reg [7:0] warp_global_id          [0:NUM_WARPS-1];
+    reg [3:0] warp_thread_count_local [0:NUM_WARPS-1];
+
+    integer i;
+    
+    always @(posedge clk) begin
+        if(reset) begin
+            for(i = 0; i < NUM_WARPS; i = i + 1) begin
+                warp_active[i]       <= 0;
+                warp_global_id[i]    <= 0;
+                warp_thread_count_local[i] <= 0;
+            end
+        end
+        else begin
+            for(i = 0; i < NUM_WARPS; i = i + 1) begin
+                if(warp_reset[i] == 1)
+                    warp_active[i] <= 0;
+                else if(warp_start[i] == 1) begin
+                    warp_active[i]             <= 1;
+                    warp_global_id[i]          <= warp_id[i];
+                    warp_thread_count_local[i] <= warp_thread_count[i];
+                end
+            end
+        end
+    end
 
     // -------------------------
     // warp scheduler
@@ -30,6 +70,7 @@ module core (
         .mem_done        (mem_done),
         .halt            (halt),
         .warp_id_from_ms (warp_id_from_ms),
+        .warp_active     (warp_active),
         .warp_id_to_ms   (warp_id_to_ms),
         .present_warp_pc (present_warp_pc),
         .present_warp_mask(),
@@ -86,6 +127,16 @@ module core (
     wire [7:0] RS1_warps [0:3][0:15];
     wire [7:0] RS2_warps [0:3][0:15];
 
+    wire [7:0] RS1_warps0 [0:15];
+    wire [7:0] RS2_warps0 [0:15];
+    genvar m;
+    generate
+        for(m=0; m<16; m=m+1) begin :warp0_assigns
+            assign RS1_warps0[m] = RS1_warps[0][m];
+            assign RS2_warps0[m] = RS2_warps[0][m];
+        end
+    endgenerate
+
     warp #(.WARP_ID(0)) warp0 (
         .clk            (clk),
         .reset          (reset),
@@ -95,13 +146,23 @@ module core (
         .reg_we         (reg_we),
         .current_warp_id(current_warp_id),
         .alu_result     (alu_result),
-        .mem_done       (mem_done),
+        .lw_ready       (lw_ready),
         .lw_warp_id     (warp_id_from_ms),
         .lw_dest_in     (lw_dest_in),
         .lw_data_in     (lw_data_in),
-        .RS1            (RS1_warps[0]),
-        .RS2            (RS2_warps[0])
+        .RS1            (RS1_warps0),
+        .RS2            (RS2_warps0)
     );
+
+    wire [7:0] RS1_warps1 [0:15];
+    wire [7:0] RS2_warps1 [0:15];
+    
+    generate
+        for(m=0; m<16; m=m+1) begin :warp1_assigns
+            assign RS1_warps1[m] = RS1_warps[1][m];
+            assign RS2_warps1[m] = RS2_warps[1][m];
+        end
+    endgenerate
 
     warp #(.WARP_ID(1)) warp1 (
         .clk            (clk),
@@ -112,13 +173,23 @@ module core (
         .reg_we         (reg_we),
         .current_warp_id(current_warp_id),
         .alu_result     (alu_result),
-        .mem_done       (mem_done),
+        .lw_ready       (lw_ready),
         .lw_warp_id     (warp_id_from_ms),
         .lw_dest_in     (lw_dest_in),
         .lw_data_in     (lw_data_in),
-        .RS1            (RS1_warps[1]),
-        .RS2            (RS2_warps[1])
+        .RS1            (RS1_warps1),
+        .RS2            (RS2_warps1)
     );
+
+    wire [7:0] RS1_warps2 [0:15];
+    wire [7:0] RS2_warps2 [0:15];
+    
+    generate
+        for(m=0; m<16; m=m+1) begin :warp2_assigns
+            assign RS1_warps2[m] = RS1_warps[2][m];
+            assign RS2_warps2[m] = RS2_warps[2][m];
+        end
+    endgenerate
 
     warp #(.WARP_ID(2)) warp2 (
         .clk            (clk),
@@ -129,13 +200,23 @@ module core (
         .reg_we         (reg_we),
         .current_warp_id(current_warp_id),
         .alu_result     (alu_result),
-        .mem_done       (mem_done),
+        .lw_ready       (lw_ready),
         .lw_warp_id     (warp_id_from_ms),
         .lw_dest_in     (lw_dest_in),
         .lw_data_in     (lw_data_in),
-        .RS1            (RS1_warps[2]),
-        .RS2            (RS2_warps[2])
+        .RS1            (RS1_warps2),
+        .RS2            (RS2_warps2)
     );
+
+    wire [7:0] RS1_warps3 [0:15];
+    wire [7:0] RS2_warps3 [0:15];
+    
+    generate
+        for(m=0; m<16; m=m+1) begin :warp3_assigns
+            assign RS1_warps3[m] = RS1_warps[3][m];
+            assign RS2_warps3[m] = RS2_warps[3][m];
+        end
+    endgenerate
 
     warp #(.WARP_ID(3)) warp3 (
         .clk            (clk),
@@ -146,12 +227,12 @@ module core (
         .reg_we         (reg_we),
         .current_warp_id(current_warp_id),
         .alu_result     (alu_result),
-        .mem_done       (mem_done),
+        .lw_ready       (lw_ready),
         .lw_warp_id     (warp_id_from_ms),
         .lw_dest_in     (lw_dest_in),
         .lw_data_in     (lw_data_in),
-        .RS1            (RS1_warps[3]),
-        .RS2            (RS2_warps[3])
+        .RS1            (RS1_warps3),
+        .RS2            (RS2_warps3)
     );
 
     // -------------------------
@@ -179,5 +260,9 @@ module core (
         .alu_result     (alu_result),
         .zero           (zero)
     );
+
+    assign lw_dest_out = A3;
+    assign warp_done[0]   = halt && (current_warp_id == 0);
+    assign warp_done[1]   = halt && (current_warp_id == 1);
 
 endmodule
